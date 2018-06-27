323baf9a6e9f18eda621505797a3dd0cea70a69c
Gemma Sharp <gs8094@bristol.ac.uk>
2018-06-27 18:43:14 +0100

    Update 4. QQ plot and lambda using Bacon.r

--- Meta_QC/4. QQ plot and lambda using Bacon.r
+++ Meta_QC/4. QQ plot and lambda using Bacon.r
@@ -1,5 +1,7 @@
 #QQ plot and Lambda using Bacon for each model
 
+time_point <- "birth" #or whatever
+
 require(bacon)
 require(gridExtra)
 
@@ -11,7 +13,7 @@ bacon.plot <- function(cohort,cohort_name){
 z.scores <- data.frame(do.call(cbind, lapply(cohort,extract.coefficients)))/data.frame(do.call(cbind, lapply(cohort,extract.se)))
 bacon.res <- apply(z.scores,2,bacon)
 bacon.lambdas <- data.frame(do.call(cbind,lapply(bacon.res,inflation)))
-filename <- paste0(cohort_name,".qqplots.outliersremoved.png")
+filename <- paste0(cohort_name,".qqplots.outliersremoved.",time_point,".png")
 png(filename, width=30,height=40,units="cm",res=300)
 plot.function<-function(i){
 	plot(bacon.res[[i]],type="qq")+ggtitle(paste(cohort_name,names(bacon.res)[[i]],"lambda =",round(bacon.lambdas[[i]],3)))+theme(legend.position="none",plot.title=element_text(hjust=0.5),axis.title=element_text(size=8))

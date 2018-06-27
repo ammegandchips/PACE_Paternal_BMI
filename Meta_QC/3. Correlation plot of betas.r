c36bc181dad52c20578909c0c97a14d884f51473
Gemma Sharp <gs8094@bristol.ac.uk>
2018-06-27 14:20:27 +0100

    Create 3. Correlation plot of betas.r

---
+++ Meta_QC/3. Correlation plot of betas.r
@@ -0,0 +1,20 @@
+# Cohort QC: Correlation plot of betas in all 12 results files
+
+require(corrplot)
+require(plyr)
+
+extract.coefficients <- function(ewas.dataframe){
+	ewas.dataframe[,which(colnames(ewas.dataframe)=="Effect")]
+}
+
+correlation.plot<-function(cohort,cohort_name){
+x <- data.frame(do.call(cbind, lapply(cohort,extract.coefficients)))
+colnames(x)<-names(list.of.results)
+filename <- paste0(cohort_name,".correlation.outliersremoved.png")
+png(filename,width=15,height=18,units="cm",res=300)
+corrplot(cor(x),method="number",type="upper")
+title(cohort_name)
+dev.off()
+}
+
+correlation.plot(list.of.results,"meta_models")

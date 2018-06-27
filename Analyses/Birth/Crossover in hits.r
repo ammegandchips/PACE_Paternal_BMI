128bc1bc2e4088276d176a96572aac9a32df7ace
Gemma Sharp <gs8094@bristol.ac.uk>
2018-06-27 15:17:08 +0100

    Update Crossover in hits.r

--- Analyses/Birth/Crossover in hits.r
+++ Analyses/Birth/Crossover in hits.r
@@ -12,3 +12,12 @@ intersect(list.of.results.het.removed$boys.patmat[which(list.of.results.het.remo
 #females patmat vs matpat
 intersect(list.of.results.het.removed$girls.patmat[which(list.of.results.het.removed$girls.patmat$Pvalue<1e-5),"MarkerName"],list.of.results.het.removed$girls.matpat[which(list.of.results.het.removed$girls.matpat$Pvalue<1e-5),"MarkerName"])#0
 
+#patmat males vs females
+patmat.girls.sites <- list.of.results.het.removed$girls.patmat[which(list.of.results.het.removed$girls.patmat$Pvalue<1e-5),]
+patmat.boys.sites <- list.of.results.het.removed$boys.patmat[which(list.of.results.het.removed$boys.patmat$Pvalue<1e-5),]
+intersect(patmat.girls.sites$MarkerName,patmat.boys.sites$MarkerName) 
+
+#matpat males vs females
+matpat.girls.sites <- list.of.results.het.removed$girls.matpat[which(list.of.results.het.removed$girls.matpat$Pvalue<1e-5),]
+matpat.boys.sites <- list.of.results.het.removed$boys.matpat[which(list.of.results.het.removed$boys.matpat$Pvalue<1e-5),]
+intersect(matpat.girls.sites$MarkerName,matpat.boys.sites$MarkerName) # 0

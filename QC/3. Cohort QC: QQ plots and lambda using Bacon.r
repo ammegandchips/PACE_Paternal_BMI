9cf79aadcb09800fd1d47df53132780683f5d552
Gemma Sharp <gs8094@bristol.ac.uk>
2018-06-27 16:29:19 +0100

    Update 3. Cohort QC: QQ plots and lambda using Bacon.r

--- QC/3. Cohort QC: QQ plots and lambda using Bacon.r
+++ QC/3. Cohort QC: QQ plots and lambda using Bacon.r
@@ -1,4 +1,7 @@
 # Cohort QC: QQ plots and lambda using Bacon
+
+time_point <- "birth" #or whatever
+
 require(bacon)
 require(gridExtra)
 extract.se <- function(ewas.dataframe){
@@ -10,7 +13,7 @@ z.scores <- data.frame(do.call(cbind, lapply(cohort,extract.coefficients)))/data
 colnames(z.scores)<-key$result[which(colnames(z.scores)%in%key$merged.title.coef)]
 bacon.res <- apply(z.scores,2,bacon)
 bacon.lambdas <- data.frame(do.call(cbind,lapply(bacon.res,inflation)))
-filename <- paste0("qc_res/",cohort_name,".qqplots.png")
+filename <- paste0("qc_res/",cohort_name,".",time_point,".qqplots.png")
 png(filename, width=30,height=40,units="cm",res=300)
 plot.function<-function(i){
 	plot(bacon.res[[i]],type="qq")+ggtitle(paste(cohort_name,names(bacon.res)[[i]],"lambda =",round(bacon.lambdas[[i]],3)))+theme(legend.position="none",plot.title=element_text(hjust=0.5),axis.title=element_text(size=8))

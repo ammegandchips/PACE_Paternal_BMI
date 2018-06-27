eb01f2ac2a7b2b6646c109cd7085c929e18f2b21
Gemma Sharp <gs8094@bristol.ac.uk>
2018-06-27 15:10:17 +0100

    Update Volcano plots.r

--- Analyses/Birth/Volcano plots.r
+++ Analyses/Birth/Volcano plots.r
@@ -46,6 +46,51 @@ ylab("-log10(P-value)")+
 theme(legend.position="none",plot.title = element_text(hjust = 0.5))+
 ggtitle("Maternal BMI adjusted for paternal BMI")
 
+PatMat.male.volcano<-ggplot(list.of.results.het.removed$boys.patmat,aes(x=Effect*100,y=-log10(Pvalue)))+
+geom_point(aes(col=Pvalue<1e-5),size=0.4)+
+scale_colour_manual(values=c("grey22","deepskyblue2"))+
+theme_classic()+
+xlab("Difference in % offspring DNA methylation\n per 1SD increase in paternal BMI")+
+#ylim(c(0,7))+
+#xlim(c(-4,4))+
+ylab("-log10(P-value)")+
+theme(legend.position="none",plot.title = element_text(hjust = 0.5))+
+ggtitle("Paternal BMI adjusted for maternal BMI (male offspring)")
+
+MatPat.male.volcano<-ggplot(list.of.results.het.removed$boys.matpat,aes(x=Effect*100,y=-log10(Pvalue)))+
+geom_point(aes(col=Pvalue<1e-5),size=0.4)+
+scale_colour_manual(values=c("grey22","deepskyblue2"))+
+theme_classic()+
+xlab("Difference in % offspring DNA methylation\n per 1SD increase in maternal BMI")+
+#ylim(c(0,7))+
+#xlim(c(-4,4))+
+ylab("-log10(P-value)")+
+theme(legend.position="none",plot.title = element_text(hjust = 0.5))+
+ggtitle("Maternal BMI adjusted for paternal BMI (male offspring)")
+
+PatMat.female.volcano<-ggplot(list.of.results.het.removed$girls.patmat,aes(x=Effect*100,y=-log10(Pvalue)))+
+geom_point(aes(col=Pvalue<1e-5),size=0.4)+
+scale_colour_manual(values=c("grey22","deepskyblue2"))+
+theme_classic()+
+xlab("Difference in % offspring DNA methylation\n per 1SD increase in paternal BMI")+
+#ylim(c(0,7))+
+#xlim(c(-4,4))+
+ylab("-log10(P-value)")+
+theme(legend.position="none",plot.title = element_text(hjust = 0.5))+
+ggtitle("Paternal BMI adjusted for maternal BMI (female offspring)")
+
+MatPat.female.volcano<-ggplot(list.of.results.het.removed$girls.matpat,aes(x=Effect*100,y=-log10(Pvalue)))+
+geom_point(aes(col=Pvalue<1e-5),size=0.4)+
+scale_colour_manual(values=c("grey22","deepskyblue2"))+
+theme_classic()+
+xlab("Difference in % offspring DNA methylation\n per 1SD increase in maternal BMI")+
+#ylim(c(0,7))+
+#xlim(c(-4,4))+
+ylab("-log10(P-value)")+
+theme(legend.position="none",plot.title = element_text(hjust = 0.5))+
+ggtitle("Maternal BMI adjusted for paternal BMI (female offspring)")
+
+
 png("Volcano.patbmi.png",width=500,height=500)
 Pat.volcano
 dev.off()
@@ -60,4 +105,20 @@ dev.off()
 
 png("Volcano.matpatbmi.png",width=500,height=500)
 MatPat.volcano
+dev.off()
+
+png("Volcano.patmatbmi.male.png",width=500,height=500)
+PatMat.male.volcano
+dev.off()
+
+png("Volcano.matpatbmi.male.png",width=500,height=500)
+MatPat.male.volcano
+dev.off()
+
+png("Volcano.patmatbmi.female.png",width=500,height=500)
+PatMat.female.volcano
+dev.off()
+
+png("Volcano.matpatbmi.female.png",width=500,height=500)
+MatPat.female.volcano
 dev.off()

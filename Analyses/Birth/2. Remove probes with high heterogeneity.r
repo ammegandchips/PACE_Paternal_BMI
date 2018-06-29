1e6d2fccb902f1a18b5ff8cc58e8618e87a448c3
Gemma Sharp <gs8094@bristol.ac.uk>
2018-06-29 09:51:35 +0100

    Update 2. Remove probes with high heterogeneity.r

--- Analyses/Birth/2. Remove probes with high heterogeneity.r
+++ Analyses/Birth/2. Remove probes with high heterogeneity.r
@@ -1,13 +1,7 @@
 Removing sites with high heterogeneity (HetPVal<1e-5)
 
-identify.het <- function(ewas.dataframe){
-  ewas.dataframe[which(ewas.dataframe$HetPVal<1e-3),"MarkerName"]
-}
-
-het.cpgs <- unique(unlist(lapply(list.of.results,identify.het)))#13424
-
 filterprobes.het <- function(ewas.dataframe){
-  ewas.dataframe <- ewas.dataframe[-which(ewas.dataframe$MarkerName %in% het.cpgs),]
+  ewas.dataframe[which(ewas.dataframe$HetPVal>1e-3),]
   ewas.dataframe
 }
 

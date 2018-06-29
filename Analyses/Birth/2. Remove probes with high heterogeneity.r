ee7aab741b6a6be6f9631d89f8e546509fe8c8e1
Gemma Sharp <gs8094@bristol.ac.uk>
2018-06-29 09:57:12 +0100

    Update 2. Remove probes with high heterogeneity.r

--- Analyses/Birth/2. Remove probes with high heterogeneity.r
+++ Analyses/Birth/2. Remove probes with high heterogeneity.r
@@ -2,7 +2,6 @@
 
 filterprobes.het <- function(ewas.dataframe){
   ewas.dataframe[which(ewas.dataframe$HetPVal>1e-3),]
-  ewas.dataframe
 }
 
 list.of.results.het.removed <- lapply(list.of.results,filterprobes.het)

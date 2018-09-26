0c451f5076702a2a8b9ce4016cdac36a3b7f8cb1
Gemma Sharp <gs8094@bristol.ac.uk>
2018-09-26 14:37:14 +0200

    Update 3. Correlation plot of betas.r

--- Meta_QC/3. Correlation plot of betas.r
+++ Meta_QC/3. Correlation plot of betas.r
@@ -12,7 +12,7 @@ extract.coefficients <- function(ewas.dataframe){
 correlation.plot<-function(cohort,cohort_name){
 x <- data.frame(do.call(cbind, lapply(cohort,extract.coefficients)))
 colnames(x)<-names(list.of.results)
-filename <- paste0(cohort_name,".correlation.outliersremoved.",time_point,".png")
+filename <- paste0(cohort_name,".correlation.",time_point,".png")
 png(filename,width=15,height=18,units="cm",res=300)
 corrplot(cor(x),method="number",type="upper")
 title(cohort_name)

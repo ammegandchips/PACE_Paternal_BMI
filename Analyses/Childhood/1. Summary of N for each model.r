945d9148ac066e9fbd7d75af0f61620e10952ad3
Gemma Sharp <gs8094@bristol.ac.uk>
2018-12-07 16:12:44 +0000

    Create 1. Summary of N for each model.r

---
+++ Analyses/Childhood/1. Summary of N for each model.r
@@ -0,0 +1,52 @@
+#Extract sample sizes for each model
+
+key <- data.frame(result=rep(c("min.pat","min.mat","min.patmat","min.matpat",
+  "covs.pat","covs.mat","covs.patmat","covs.matpat",
+  "boys.patmat","boys.matpat","girls.patmat","girls.matpat"),5),
+  model=rep(c("ewas.res.min.pat","ewas.res.min.mat", "ewas.res.min.mutual","ewas.res.min.mutual",
+  "ewas.res.covs.pat" , "ewas.res.covs.mat", "ewas.res.covs.mutual","ewas.res.covs.mutual",
+"ewas.res.covs.mutual.boys.only","ewas.res.covs.mutual.boys.only","ewas.res.covs.mutual.girls.only","ewas.res.covs.mutual.girls.only"),5),
+  column.suffix=rep(c("pheno.data.Zpat.bmi","pheno.data.Zmat.bmi","pheno.data.Zpat.bmi","pheno.data.Zmat.bmi",
+    "pheno.data.Zpat.bmi","pheno.data.Zmat.bmi","pheno.data.Zpat.bmi","pheno.data.Zmat.bmi",
+    "pheno.data.Zpat.bmi","pheno.data.Zmat.bmi","pheno.data.Zpat.bmi","pheno.data.Zmat.bmi"),5),
+  column.prefix=rep(c("probeid","coef","se","p","n"),each=12),
+merged.title.coef=c("ewas.res.min.pat","ewas.res.min.mat","ewas.res.min.mutual.coef.pheno.data.Zpat.bmi","ewas.res.min.mutual.coef.pheno.data.Zmat.bmi","ewas.res.covs.pat","ewas.res.covs.mat" ,"ewas.res.covs.mutual.coef.pheno.data.Zpat.bmi","ewas.res.covs.mutual.coef.pheno.data.Zmat.bmi","ewas.res.covs.mutual.boys.only.coef.pheno.data.Zpat.bmi" ,"ewas.res.covs.mutual.boys.only.coef.pheno.data.Zmat.bmi" ,"ewas.res.covs.mutual.girls.only.coef.pheno.data.Zpat.bmi","ewas.res.covs.mutual.girls.only.coef.pheno.data.Zmat.bmi"),
+merged.title.se=c("ewas.res.min.pat","ewas.res.min.mat","ewas.res.min.mutual.se.pheno.data.Zpat.bmi","ewas.res.min.mutual.se.pheno.data.Zmat.bmi","ewas.res.covs.pat","ewas.res.covs.mat" ,"ewas.res.covs.mutual.se.pheno.data.Zpat.bmi","ewas.res.covs.mutual.se.pheno.data.Zmat.bmi","ewas.res.covs.mutual.boys.only.se.pheno.data.Zpat.bmi" ,"ewas.res.covs.mutual.boys.only.se.pheno.data.Zmat.bmi" ,"ewas.res.covs.mutual.girls.only.se.pheno.data.Zpat.bmi","ewas.res.covs.mutual.girls.only.se.pheno.data.Zmat.bmi"),
+merged.title.p=c("ewas.res.min.pat","ewas.res.min.mat","ewas.res.min.mutual.p.pheno.data.Zpat.bmi","ewas.res.min.mutual.p.pheno.data.Zmat.bmi","ewas.res.covs.pat","ewas.res.covs.mat" ,"ewas.res.covs.mutual.p.pheno.data.Zpat.bmi","ewas.res.covs.mutual.p.pheno.data.Zmat.bmi","ewas.res.covs.mutual.boys.only.p.pheno.data.Zpat.bmi" ,"ewas.res.covs.mutual.boys.only.p.pheno.data.Zmat.bmi" ,"ewas.res.covs.mutual.girls.only.p.pheno.data.Zpat.bmi","ewas.res.covs.mutual.girls.only.p.pheno.data.Zmat.bmi"),
+  stringsAsFactors=FALSE)
+
+extractEWASres <- function(filename){
+  load(filename)
+  cohort.res <- mget(intersect(unique(key$model),ls()))
+  names(cohort.res) <- intersect(unique(key$model),ls())
+  cohort.res
+}
+
+ALSPAC <- extractEWASres("/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/alspac/results/ALSPAC.patbmi.ewasresults.late_childhood.Rdata")
+GenerationR <- extractEWASres("/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/GenR/GenR.patbmi.ewasresults.late_childhood.Rdata")
+CHAMACOS <- extractEWASres("/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/CHAMACOS_results/CHAMACOS.patbmi.ewasresults.late_childhood.Rdata")
+HELIX <- extractEWASres("/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/HELIX/HELIX.patbmi.ewasresults.late_childhood.Rdata")
+INMA <- extractEWASres("/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/results_PACE_INMA/4years_nocombat/PACE.patbmi.ewasresults.early_childhoodnocombat.Rdata")
+ProjectViva <- extractEWASres("/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/VIVA_PATBMI/VIVA_PATBMI/Viva.patbmi.ewasresults.early_childhood.Rdata")
+
+All.EWAS <- list(ALSPAC,CHAMACOS,GenerationR,HELIX,INMA,ProjectViva)
+names(All.EWAS) <- c("ALSPAC","CHAMACOS","GenerationR","HELIX","INMA","ProjectViva")
+
+
+extract.Ns <- function(ewas.dataframe){
+  summary(ewas.dataframe$n)
+}
+
+All.Ns<-lapply(All.EWAS,function(x) do.call(rbind,lapply(x,extract.Ns)))
+names(All.Ns) <- c("ALSPAC","CHAMACOS","GenerationR","HELIX","INMA","ProjectViva")
+Ns.covs<-do.call(rbind,lapply(All.Ns,function(x) x[which(row.names(x) =="ewas.res.covs.pat"),]))
+Ns.covs.mutual.boys.only<-do.call(rbind,lapply(All.Ns,function(x) x[which(row.names(x) =="ewas.res.covs.mutual.boys.only"),]))
+Ns.covs.mutual.girls.only<-do.call(rbind,lapply(All.Ns,function(x) x[which(row.names(x) =="ewas.res.covs.mutual.girls.only"),]))
+
+sum(Ns.covs[,6]) #1982
+sum(Ns.covs.mutual.boys.only[,6]) #1022
+sum(Ns.covs.mutual.girls.only[,6]) #960
+                                                
+write.csv(Ns.covs,"/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/meta/meta_results/childhood.samplesize_covs.csv")
+write.csv(Ns.covs.mutual.boys.only,"/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/meta/meta_results/childhood.samplesize_boys.csv")
+write.csv(Ns.covs.mutual.girls.only,"/panfs/panasas01/sscm/gs8094/EWAS/pat_bmi/meta/meta_results/childhood.samplesize_girls.csv")
